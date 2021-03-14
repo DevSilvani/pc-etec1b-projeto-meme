61530598c240b527e14e56ad4ddaf33f1ee6de89
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-14 20:57:15 -0300

    C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_meme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "";
+            Console.Write("digite o que você acha sobre este programa: ");
+            frase = Console.ReadLine();
+            Console.Clear();
+            frase = frase.Substring(0,10);
+            Console.WriteLine(frase+"... NÃO, PERA.\nTô nervoso");
+            Console.ReadKey();
+        }
+    }
+}
